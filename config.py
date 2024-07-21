181ef9614895b63197bd87303493de1dcf915b47
ShaneZD <146131214+ShaneZD@users.noreply.github.com>
2024-07-21 21:13:40 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'a_default_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql://postgres:postgres@localhost/rigster')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///rigster.db')  # Using SQLite instead, couldn't get it working
     SQLALCHEMY_TRACK_MODIFICATIONS = False
